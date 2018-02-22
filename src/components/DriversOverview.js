ee31a3fdc66a0b9a02afa8cc394edef523da0f31
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-22 21:33:32 +0100

    display drivers info and random image

--- src/components/DriversOverview.js
+++ src/components/DriversOverview.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// material-ui
+import { withStyles } from 'material-ui/styles';
+import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
+import Subheader from 'material-ui/List/ListSubheader';
+import IconButton from 'material-ui/IconButton';
+import InfoIcon from 'material-ui-icons/Info';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: '100vw',
+    height: '100vh',
+  },
+  subheader: {
+    width: '100%',
+  },
+});
+
 class DriversOverview extends React.Component {
   constructor(props, context) {
     super(props, context);
@@ -21,9 +45,28 @@ class DriversOverview extends React.Component {
   }
 
   render() {
+    const { drivers, classes, } = this.props;
+    // https://source.unsplash.com/collection/887947/
     return (
       <div  className="drivers-overview-container">
-
+        <div className={classes.root}>
+          <GridList cellHeight={360} className={classes.gridList} cols={3}>
+            {drivers.map(driver => (
+              <GridListTile key={driver.driverId} cols={driver.wins || 1}>
+                <img src={`https://source.unsplash.com/300x200/?ferrari,${driver.familyName}`} alt={driver.name} />
+                <GridListTileBar
+                  title={`${driver.familyName} ${driver.givenName}`}
+                  subtitle={<span>{driver.nationality}</span>}
+                  actionIcon={
+                    <IconButton className={classes.icon}>
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
       </div>
     )
   }
@@ -38,4 +81,4 @@ DriversOverview.propTypes = {
   fetchChampionshipWinners: PropTypes.func.isRequired,
 };
 
-export default DriversOverview;
+export default withStyles(styles)(DriversOverview);
