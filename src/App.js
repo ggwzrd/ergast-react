ceaa7c6ac71d0e17e60a062f552caf6029512f1b
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-22 21:07:11 +0100

    add DriverOverview component to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
+
+// components
+import DriversOverview from './containers/DriversOverviewContainer';
+
+// images
 import logo from './logo.svg';
+
+// styles
 import './App.css';
 
 class App extends Component {
@@ -10,9 +17,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <DriversOverview />
       </div>
     );
   }
