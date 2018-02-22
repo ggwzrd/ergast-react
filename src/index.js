a5b0f5f49c8ae900745b2bd8acfb90c19b477e19
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-22 19:43:14 +0100

    add redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store'
+
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
