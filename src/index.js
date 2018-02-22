8d28979bb6725f372fde7b70fd62d326e88f15d1
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-22 19:48:00 +0100

    wrap App in a Redux Provider component

--- src/index.js
+++ src/index.js
@@ -8,5 +8,10 @@ import store from './store'
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker();
