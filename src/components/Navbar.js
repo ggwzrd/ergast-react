939cfe19021e80b186630819072249e395da261a
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-23 18:42:42 +0100

    update navbar style with website palette

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,25 +7,55 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Radio from 'material-ui/Radio';
+import IconButton from 'material-ui/IconButton';
 import { FormControlLabel, } from 'material-ui/Form';
+import Tooltip from 'material-ui/Tooltip';
+
+// material-ui icons
+import FiltersIcon from 'material-ui-icons/FilterList';
 
 // styles
+import './Navbar.css';
+
 const styles = {
   root: {
     justifyContent: 'space-between',
-  }
+    alignItems: 'center',
+    minHeight: '50px',
+    height: '50px',
+    backgroundColor: '#424242',
+  },
+  menuButton: {
+    marginLeft: -12,
+  },
 }
 
 class Navbar extends React.Component {
   render() {
-    const { handleHiglight, highlighted, classes, } = this.props;
+    const { handleHiglight, highlighted, classes, toggleFilters, } = this.props;
 
     return (
-      <AppBar position="fixed" color="default" >
+      <AppBar position="fixed" color="primary">
         <Toolbar className={classes.root}>
-          <Typography variant="title" color="inherit">
-            Ferrari Drivers
-          </Typography>
+          <div className="title-container">
+            <Tooltip id="tooltip-filters" title="Filters" placement="bottom">
+              <IconButton
+                onClick={toggleFilters(true)}
+                className={classes.menuButton}
+                color="primary"
+                aria-label="Filters">
+                <FiltersIcon />
+              </IconButton>
+            </Tooltip>
+
+            <Typography
+              variant="title"
+              color="primary"
+              style={{ display: 'inline-block', }}
+            >
+              Ferrari Drivers
+            </Typography>
+          </div>
           <div className="right-buttons">
             <FormControlLabel
               value="winners"
@@ -64,6 +94,7 @@ Navbar.defaultProps = {
 Navbar.propTypes = {
   handleHiglight: PropTypes.func.isRequired,
   highlighted: PropTypes.string,
+  toggleFilters: PropTypes.func.isRequired,
 }
 
 export default withStyles(styles)(Navbar);
