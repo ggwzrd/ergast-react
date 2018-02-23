ca7c5ba81e9bb7bcc6d084d5ee2c9a03d824f7c9
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-23 18:40:04 +0100

    make sure to fetch all the results from the beginning of history

--- src/actions/fetch-results.js
+++ src/actions/fetch-results.js
@@ -1,30 +1,41 @@
 import request from 'axios';
 
 export const RESULTS_FETCHED = 'RESULTS_FETCHED';
+export const RACES_SAVED = 'RACES_SAVED';
 
 export const resultsFetched = (winners) => ({
   type: RESULTS_FETCHED,
   payload: winners,
 });
 
+export const racesSaved = (races) => ({
+  type: RACES_SAVED,
+  payload: races,
+});
+
 const getWinners = (races) => {
-  return races.map((race) => race.Results[0].Driver);
+  return races.map((race) =>
+    race.Results.reduce((winner, result) => !winner && result.position === '1' ? result.Driver : winner, false), false
+  )
 }
 
 export default () => {
   return dispatch => {
-    request({
-      method: 'GET',
-      url: 'https://ergast.com/api/f1/constructors/ferrari/results/1.json?limit=300',
-      responseType: 'json',
-    })
-      .then((res) => {
-        const data    = res.data.MRData;
-        const races   = data.RaceTable.Races
-        const winners = getWinners(races);
-
-        dispatch(resultsFetched(winners));
+    for(let o = 0; o <= 2140; o+=300)
+      request({
+        method: 'GET',
+        url: `https://ergast.com/api/f1/constructors/ferrari/results.json?offset=${o}&limit=300`,
+        responseType: 'json',
       })
-      .catch(console.warn);
+        .then((res) => {
+          const data    = res.data.MRData;
+          const races   = data.RaceTable.Races
+          const winners = getWinners(races);
+
+          console.log(races);
+          dispatch(resultsFetched(winners));
+          dispatch(racesSaved(races));
+        })
+        .catch(console.warn);
   }
 };
