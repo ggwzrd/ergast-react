8ded35818ca16a5f10aa37192983f7f7f74edf18
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-23 02:15:07 +0100

    merge all the data got from the action to the drivers and calculate what's necesary

--- src/reducers/drivers.js
+++ src/reducers/drivers.js
@@ -9,11 +9,43 @@ const INITIAL_STATE =  {
   all: [],
 };
 
+const updateWinners = (state, winners) => {
+  const drivers        = state.all;
+
+  // map through the drivers and assign to each of them the respective victories
+  const updatedDrivers = drivers.map((driver) => Object.assign({}, driver, {
+    victories: assignWins(driver.driverId, winners)
+  }))
+
+  return Object.assign({}, state, { all: updatedDrivers})
+}
+
+// loop though all the winners count only the ones gained from the current driver
+const assignWins = (id, winners) => {
+  return winners.reduce((victories, driver) => driver.driverId === id ? victories + 1 : victories, 0);
+}
+
+const assignChampionships = (state, champions) => {
+  const drivers        = state.all
+  const updatedDrivers = drivers.map((driver) =>
+    Object.assign({}, driver, {
+      wordChampion: champions.indexOf(driver.driverId) >= 0
+    })
+  );
+  
+  return Object.assign({}, state, { all: updatedDrivers, });
+}
+
 export default (state = INITIAL_STATE, { type, payload } = {}) => {
 
   switch (type) {
     // using Object assign for avoid the mutation of the state
     case DRIVER_FETCHED: return Object.assign({}, state, { ...payload, });
+
+    // update drivers with the data of the winners
+    case RESULTS_FETCHED: return updateWinners(state, payload);
+
+    case CHAMPIONSHIP_WINNERS_FETCHED: return assignChampionships(state, payload);
 
     default: return state;
   }
