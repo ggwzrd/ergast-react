b7fb602f2cabd6a4c1c7de6032e9e20ff6083bba
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-23 18:44:41 +0100

    add theme to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 
+// material-ui
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+
 // components
 import DriversOverview from './containers/DriversOverviewContainer';
 
 // styles
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      light: '#ff655d',
+      main: '#ff2332',
+      dark: '#c3000a',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#ffbf81',
+      main: '#ff8e53',
+      dark: '#c75f27',
+      contrastText: '#000000',
+    },
+  },
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <DriversOverview />
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <DriversOverview />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
