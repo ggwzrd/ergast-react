bf6e0e9244e2c0da2ee3dfcbc3aff37e45d9409f
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-23 02:33:33 +0100

    fetch all the championship winners and filter them for constructor (ferrari)

--- src/actions/fetch-championship-winners.js
+++ src/actions/fetch-championship-winners.js
@@ -2,22 +2,34 @@ import request from 'axios';
 
 export const CHAMPIONSHIP_WINNERS_FETCHED = 'CHAMPIONSHIP_WINNERS_FETCHED';
 
-export const championshipWinnersFetched = (data) => ({
+export const championshipWinnersFetched = (championshipWinners) => ({
   type: CHAMPIONSHIP_WINNERS_FETCHED,
-  payload: data,
+  payload: championshipWinners,
 });
+
+const getChampionshipWinners = (championships) => {
+
+  return championships.map((championship) => {
+    const constructor = championship.DriverStandings[0].Constructors[0].constructorId;
+    const driverId    = championship.DriverStandings[0].Driver.driverId;
+
+    return constructor === 'ferrari' ? driverId : false
+  }).filter((driver) => driver);
+}
 
 export default () => {
   return dispatch => {
     request({
       method: 'GET',
-      url: `http://ergast.com/api/f1/constructors/ferrari/constructorStandings/1/drivers`,
+      url: 'http://ergast.com/api/f1/driverStandings/1.json?limit=300',
       responseType: 'json',
     })
       .then((res) => {
-        console.log(res);
-        debugger
-        dispatch(championshipWinnersFetched(res.data));
+        const data                = res.data.MRData;
+        const champioships        = data.StandingsTable.StandingsLists
+        const championshipWinners = getChampionshipWinners(champioships);
+
+        dispatch(championshipWinnersFetched(championshipWinners));
       })
       .catch(console.warn);
   }
