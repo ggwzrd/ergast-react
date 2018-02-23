5e4f44f4f93eea20403ec30fb8eba9e5af9f5b12
Giulio Gallerini <giulio.gallerini.work@hotmail.com>
2018-02-23 02:37:00 +0100

    render drivers overview in App

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 // components
 import DriversOverview from './containers/DriversOverviewContainer';
 
-// images
-import logo from './logo.svg';
-
 // styles
 import './App.css';
 
@@ -13,10 +10,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <DriversOverview />
       </div>
     );
